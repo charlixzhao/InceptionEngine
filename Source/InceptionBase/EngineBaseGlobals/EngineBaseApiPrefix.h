5a5a48e02558bd639b43c07d430890c8fd74b2f7
fhh200000 <1041154549@qq.com>
2021-05-29 12:13:32 +0800

    Bug Fix:Change dllexport.

--- Source/InceptionBase/EngineBaseGlobals/EngineBaseApiPrefix.h
+++ Source/InceptionBase/EngineBaseGlobals/EngineBaseApiPrefix.h
@@ -1,8 +1,10 @@
-
-/*#pragma once
-
+#pragma once
+#ifdef WIN32
 #ifdef InceptionBase_EXPORTS
 #define IE_Base_API __declspec(dllexport)
 #else
 #define IE_Base_API __declspec(dllimport)
-#endif*/
+#endif
+#elif defined __linux__
+#define IE_Base_API
+#endif
