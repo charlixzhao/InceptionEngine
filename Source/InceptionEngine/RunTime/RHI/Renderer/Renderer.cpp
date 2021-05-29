0fff3d73d23d140649fda264284cf084e58c183b
fhh200000 <1041154549@qq.com>
2021-05-29 11:08:17 +0800

    Bug Fix: SIGSEGV when vkCreateSwapchainKHR() creates more image buffers than needed.

--- Source/InceptionEngine/RunTime/RHI/Renderer/Renderer.cpp
+++ Source/InceptionEngine/RunTime/RHI/Renderer/Renderer.cpp
@@ -122,34 +122,28 @@ namespace inceptionengine
 			std::cerr << "Fail to get next available image from the swap chain" << std::endl;
 			throw std::runtime_error("");
 		}
-
 		vkWaitForFences(m_device, 1, &m_acquireImageFence, VK_TRUE, UINT64_MAX);
-
 		vkResetFences(m_device, 1, &m_acquireImageFence);
-
-		vkWaitForFences(m_device, 1, &m_imageFinishRenderFence[m_nextRenderImgIdx], VK_TRUE, UINT64_MAX);
-
-		vkResetFences(m_device, 1, &m_imageFinishRenderFence[m_nextRenderImgIdx]);
-
-		RecordCommandBuffer(m_nextRenderImgIdx);
-
+		vkWaitForFences(m_device, 1, &m_imageFinishRenderFence[m_nextRenderImgIdx%NumOfRenderBuffers], VK_TRUE, UINT64_MAX);
+		vkResetFences(m_device, 1, &m_imageFinishRenderFence[m_nextRenderImgIdx%NumOfRenderBuffers]);
+		RecordCommandBuffer(m_nextRenderImgIdx%NumOfRenderBuffers);
 		VkSubmitInfo submitInfo = { VK_STRUCTURE_TYPE_SUBMIT_INFO };
 		VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
 		submitInfo.pWaitDstStageMask = waitStages;
 		submitInfo.commandBufferCount = 1;
-		submitInfo.pCommandBuffers = &m_commandBuffers[m_nextRenderImgIdx];
+		submitInfo.pCommandBuffers = &m_commandBuffers[m_nextRenderImgIdx%NumOfRenderBuffers];
 		submitInfo.signalSemaphoreCount = 1;
-		submitInfo.pSignalSemaphores = &m_graphicsFinishSemaphores[m_nextRenderImgIdx];
-		vkQueueSubmit(m_queues.at(GraphicsQueue), 1, &submitInfo, m_imageFinishRenderFence[m_nextRenderImgIdx]);
+		submitInfo.pSignalSemaphores = &m_graphicsFinishSemaphores[m_nextRenderImgIdx%NumOfRenderBuffers];
+		vkQueueSubmit(m_queues.at(GraphicsQueue), 1, &submitInfo, m_imageFinishRenderFence[m_nextRenderImgIdx%NumOfRenderBuffers]);
 
 		VkPresentInfoKHR presentInfo = { VK_STRUCTURE_TYPE_PRESENT_INFO_KHR };
 		presentInfo.waitSemaphoreCount = 1;
-		presentInfo.pWaitSemaphores = &m_graphicsFinishSemaphores[m_nextRenderImgIdx];
+		presentInfo.pWaitSemaphores = &m_graphicsFinishSemaphores[m_nextRenderImgIdx%NumOfRenderBuffers];
 		presentInfo.swapchainCount = 1;
 		presentInfo.pSwapchains = &mSwapchain;
+        m_nextRenderImgIdx%=NumOfRenderBuffers;
 		presentInfo.pImageIndices = &m_nextRenderImgIdx;
 		vkQueuePresentKHR(m_queues.at(PresentQueue), &presentInfo);
-
 	}
 
 
@@ -658,7 +652,7 @@ namespace inceptionengine
 		fenceCreateInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 		m_graphicsFinishSemaphores.resize((size_t)NumOfRenderBuffers);
 		m_imageFinishRenderFence.resize((size_t)NumOfRenderBuffers);
-
+        std::cout << "NumberOfRenderBuffers:"<<NumOfRenderBuffers<<std::endl;
 		for (size_t i = 0; i < NumOfRenderBuffers; i++)
 		{
 			vkCreateSemaphore(m_device, &createInfo, nullptr, &m_graphicsFinishSemaphores[i]);
@@ -1200,9 +1194,7 @@ namespace inceptionengine
 
 		auto transformCopy = transformations;
 
-		unsigned int writeToImageIdx = m_nextRenderImgIdx ^ 1;
-
-
+		unsigned int writeToImageIdx = (m_nextRenderImgIdx%NumOfRenderBuffers) ^ 1;
 		void* data = nullptr;
 
 		vkMapMemory(m_device, uBuffer.boneTransformMemory[writeToImageIdx], 0, sizeof(Matrix4x4f) * transformations.size(), 0, &data);
